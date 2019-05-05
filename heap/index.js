5852b1898259907fcbbb8c89893fdd8c7761a95b
parenparen <dtmuelle@gmail.com>
2019-05-05 11:35:20 -0700

    add comparison function to heap and fix bug

--- heap/index.js
+++ heap/index.js
@@ -1,11 +1,12 @@
 
 class Heap {
-  constructor() {
+  constructor(compare = (a, b) => a < b) {
     this.arr = [];
+    this.compare = compare; 
   }
 
   getParent(i) {
-    return Math.floor(i / 2);
+    return Math.floor((i - 1) / 2);
   }
 
   getLeft(i) {
@@ -21,13 +22,21 @@ class Heap {
     this.arr[i] = this.arr[j];
     this.arr[j] = tmp;
   }
+    
+  getTop() {
+      return this.arr[0];
+  }  
+    
+  isEmpty() {
+      return !this.arr.length;
+  }  
 
   insert(val) {
     this.arr.push(val);
     let i = this.arr.length - 1,
         p = this.getParent(i);
 
-    while(i > 0 && this.arr[i] < this.arr[p]) {
+    while(i > 0 && this.compare(this.arr[i], this.arr[p])) {
       this.swap(p, i);
       i = p;
       p = this.getParent(i);
@@ -49,11 +58,11 @@ class Heap {
 
     while(left < this.arr.length || right < this.arr.length) {
       if(right < this.arr.length) {
-        if(this.arr[i] > this.arr[left] && 
-          this.arr[i] > this.arr[right]) { 
+        if(this.compare(this.arr[i], this.arr[left]) && 
+          this.compare(this.arr[i], this.arr[right])) { 
 
           break;
-        } else if (this.arr[left] < this.arr[right]) {
+        } else if (this.compare(this.arr[left], this.arr[right])) {
           this.swap(i, left);
           i = left;
         } else {
@@ -61,7 +70,7 @@ class Heap {
           i = right;
         }
       } else {
-        if(this.arr[i] < this.arr[left]) {
+        if(this.compare(this.arr[i], this.arr[left])) {
           break;
         } else {
           this.swap(i, left);
--- heap/index.test.js
+++ heap/index.test.js
@@ -2,22 +2,45 @@
 const {Heap} = require('./');
 
 describe('Heap', () => {
-  describe('insert', () => {
-    it('returns undefined', () => {
-      let val = 1;
-      expect((new Heap).insert(val)).toEqual(undefined);
+  describe('min heap', () => {
+    describe('insert', () => {
+      it('returns undefined', () => {
+        let val = 1;
+        expect((new Heap).insert(val)).toEqual(undefined);
+      });
+    });
+
+    describe('extract', () => {
+      it('returns smallest value', () => {
+        const heap = new Heap;
+        heap.insert(3);
+        heap.insert(2);
+        heap.insert(1);
+        expect(heap.extract()).toEqual(1);
+        expect(heap.extract()).toEqual(2);
+        expect(heap.extract()).toEqual(3);
+      });
     });
   });
 
-  describe('extract', () => {
-    it('returns smallest value', () => {
-      const heap = new Heap;
-      heap.insert(3);
-      heap.insert(2);
-      heap.insert(1);
-      expect(heap.extract()).toEqual(1);
-      expect(heap.extract()).toEqual(2);
-      expect(heap.extract()).toEqual(3);
+  describe('max heap', () => {
+    describe('insert', () => {
+      it('returns undefined', () => {
+        let val = 1;
+        expect((new Heap((a, b) => a > b)).insert(val)).toEqual(undefined);
+      });
+    });
+
+    describe('extract', () => {
+      it('returns smallest value', () => {
+        const heap = new Heap((a, b) => a > b);
+        heap.insert(1);
+        heap.insert(2);
+        heap.insert(3);
+        expect(heap.extract()).toEqual(3);
+        expect(heap.extract()).toEqual(2);
+        expect(heap.extract()).toEqual(1);
+      });
     });
   });
 });
